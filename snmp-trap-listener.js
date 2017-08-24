0142767a84ff429db59021c72355f3d45994771f
Marc Berchtold <marc.berchtold@hotmail.de>
2017-08-24 14:25:54 +0200

    Basic trap listener functionality

--- snmp-trap-listener.js
+++ snmp-trap-listener.js
@@ -1,3 +1,6 @@
+const util              = require('util');
+const snmp              = require('snmpjs');
+
 module.exports = (RED, debugSettings) => {
     if (!RED && debugSettings) {
         // Enter debug mode | Run outside of Node-RED
@@ -5,15 +8,45 @@ module.exports = (RED, debugSettings) => {
 
     function SnmpTrapListenerNode(config) {
         RED.nodes.createNode(this,config);
-        let node = this;
+        if (!config.port)
+            config.port = 161;
 
-        node.on('close', done => {
-            // Cleanup before node is being deleted. Call done when finished.
-            done();
+        let node = this;
+        let timeoutStatus;
+        node.status({fill: 'yellow', shape: 'dot', text: 'connecting'});
+        const trapd = snmp.createTrapListener();
+
+        node.on('close', function() {
+            // Cleanup before node is being deleted.
+            trapd.close();
         });
 
-        node.send({payload: 'Test'});
-        node.status({fill: 'green', shape: 'dot', text: 'ready'});
+        // console.dir(config);
+        trapd.on('trap', msg => {
+            clearTimeout(timeoutStatus);
+            node.status({fill: 'blue', shape: 'dot', text: 'ready - trapped'});
+            timeoutStatus = setTimeout(() => {
+                node.status({fill: 'green', shape: 'dot', text: 'ready'});
+            }, 50);
+
+            const payload = [];
+            const serializedMsg = snmp.message.serializer(msg)['pdu'];
+
+            serializedMsg.varbinds.forEach(variable => {
+                const oids = config.oids.split('\n');
+
+                if (oids.indexOf(variable.oid) !== -1) {
+                    payload.push(variable);
+                }
+            });
+
+            if (payload.length > 0)
+                node.send({payload});
+        });
+
+        trapd.bind({family: 'udp4', port: parseInt(config.port)}, () => {
+            node.status({fill: 'green', shape: 'dot', text: 'ready'});
+        });
     }
 
     if (RED)
