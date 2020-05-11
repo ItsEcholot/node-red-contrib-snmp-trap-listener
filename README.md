11ef0d00e28e68d99cc9f00d662ac6d1fdcfb5e5
Chris <aidasan@hotmail.fr>
2020-05-12 00:18:50 +0900

    v2.0.0
    Change: dependency from "snmpjs" to "net-snmp"
    Add: snmp version selection (v1, v2c, v3)
    Add: v3 security
    New: IP filtering function with 2 different type of netmask
    New: log system (node with node-red and console without)
    Bonus: it won't make node-red crash if the port can't be opened.

--- README.md
+++ README.md
@@ -3,31 +3,92 @@ Node-RED SNMP Trap Listener
 
 [![NPM](https://nodei.co/npm/node-red-contrib-snmp-trap-listener.png)](https://nodei.co/npm/node-red-contrib-snmp-trap-listener/)
 
-This Node-RED node will bind itself to the configured port and start listening for SNMP Trap packages
-that meet the filter settings.
+This Node-RED node listen for SNMP Trap packages on the configured port.  
+Trap which do meet the filter setting will emitted on the node output.
 
-One can filter the received data by ip + netmask and/or by a community string.
+The filter option are:  
+SNMP version (v1, v2c, v3)  
+Community string (v1 & v2c only)  
+User credential (v3 only)  
+IP Filtering  
 
 Getting Started
 ---------------
-Navigate to the node-red installation directory and add the package via NPM.
-```
-cd $HOME/.node-red
-npm install node-red-contrib-snmp-trap-listener
-```
-Restart node-red and reload your node-red browser window for changes to take effect.
-Remember to configure the snmp-trap-listener node before you deploy your changes.
+[Adding nodes to the palette](https://nodered.org/docs/user-guide/runtime/adding-nodes) - Node-RED Documentation  
+Remember to configure the **snmp-trap-listener** node before you deploy your changes.
 
 Documentation
 -------------
 ### Input
-The snmp-trap-listener doesn't take any input.
+The **snmp-trap-listener** node doesn't take any input.
 
 ### Output
-The emitted payload object contains an array which contains all received variable bindings
-in the following format:
+The **snmp-trap-listener** node output object is in the following format:
 
+SNMP v1
+```json
+{
+	"payload":[
+		{
+			"oid":"1.3.6.1.4.1.13576.10.1.40.4.4.2",
+			"typename":"OctetString",
+			"value":[80,108,99,32,83,116,97,114,116],
+			"string_value":"Plc Start"
+		}
+	],
+	"enterprise":"1.3.6.1.4.1.13576",
+	"agent_addr":"10.0.1.125",
+	"generic_trap":6,
+	"specific_trap":2,
+	"time_stamp":8932,
+	"src":{
+		"family":"IPv4",
+		"address":"10.0.1.125",
+		"port":44688
+	},
+	"version":0,
+	"string_version":"Trap",
+	"_msgid":"e7f3490e.2320d8"
+}
 ```
+
+SNMP v2c/v3
+```json
+{
+	"payload":[
+		{
+			"oid":"1.3.6.1.2.1.1.3.0",
+			"typename":"TimeTicks",
+			"value":760,
+			"string_value":"760"
+		},
+		{
+			"oid":"1.3.6.1.6.3.1.1.4.1.0",
+			"typename":"OID",
+			"value":"1.3.6.1.4.1.13576.0.2",
+			"string_value":"1.3.6.1.4.1.13576.0.2"
+		},
+		{
+			"oid":"1.3.6.1.4.1.13576.10.1.40.4.4.2",
+			"typename":"OctetString",
+			"value":[80,108,99,32,83,116,97,114,116],
+			"string_value":"Plc Start"
+		}
+	],
+	"src":{
+		"family":"IPv4",
+		"address":"10.0.1.125",
+		"port":28391
+	},
+	"version":1,
+	"string_version":"TrapV2",
+	"_msgid":"d9562ebf.2f62d"
+}
+```
+
+It's payload contains an array which contains all received variable bindings.
+
+```javascript
 {
     oid: "1.3.1",                   //OID path
     typename: "OctetString",        //Typename
@@ -40,48 +101,49 @@ in the following format:
 ### Options
 The node contains the following options:
 - Port - Sets the port on which to listen for SNMP traps
-- Community - If set every package get's checked if it contains the same community string as set.  
-If it doesnt't, the package get's denied and a debug message is emitted.
-- IP Filter - If set every package get's checked if its origin matches the IP Filter made from the
-IP and the mask set.
+- SNMP Version - Select which version of SNMP should be used.
+- SNMP v1/v2c:
+  - Community - Network community string. The community string acts like a user ID or password. A user with the correct community string has access to network information. The default is public.
+- SNMP v3
+  - Username - Username of the SNMP User-based Security Model (USM) user.
+  - Authentication Protocol - Authentication protocol used to authenticate messages sent on behalf of the specified Username.
+  - Authentication Key - Authentication key used by the selected authentication protocol.
+  - Encryption Protocol - Encryption protocol used to encrypt messages sent on behalf of the specified Username.
+  - Encryption Key - Encryption key used by the selected encryption protocol.
+- IP Filter:
+  - IP Address - Base IP address used for the filter.
+  - Network mask - Network mask used to calculate the filter IP address range.  
+   It can be in four-part dotted-decimal format: 255.255.255.0  
+   or in CIDR (Classless Inter-Domain Routing) notation: 24  
 
-Sample Output Payload
----------------------
-```json
-[
-  {
-    "oid": "1.3.1.1",
-    "typename": "OctetString",
-    "value": {
-      "type": "Buffer",
-      "data": [
-        116,
-        101,
-        115,
-        116,
-        86,
-        97,
-        108,
-        117,
-        101
-      ]
-    },
-    "string_value": "testValue"
-  },
-  {
-    "oid": "1.3.1.1",
-    "typename": "Integer",
-    "value": 5,
-    "string_value": "5"
-  }
-]
-```
+  Example of IP filter:
+  | IP Address | Network mask | Filter Start | Filter End |
+  |---|---|---|---|
+  | 192.168.1.1 | 24 | 192.168.1.1 | 192.168.1.254 |
+  | 192.168.1.1 | 255.255.255.0 | 10.0.1.1 | 10.0.1.254 |
+  | 10.0.1.25 | 30 | 10.0.1.25 | 10.0.1.26 |
+  | 10.0.1.25 | 255.255.255.252 | 10.0.1.25 | 10.0.1.26 |
 
 Debugging
 ---------
-Run index.js with node to start the listener in your debugger of choice.
+Run `node index.js` to start the listener in your debugger of choice.
 
-Built with / Dependencies
--------------------------
-- [snmpjs](https://github.com/joyent/node-snmpjs) - SNMP library
-- [moment](https://github.com/moment/moment) - Timestamp format library
+Project Informations
+--------------------
+
+### - Current version:
+![GitHub package.json version](https://img.shields.io/github/package-json/v/ItsEcholot/node-red-contrib-snmp-trap-listener)
+
+### - Vulnerabilities:
+![Snyk Vulnerabilities for GitHub Repo](https://img.shields.io/snyk/vulnerabilities/github/ItsEcholot/node-red-contrib-snmp-trap-listener)
+
+### - Languages:
+![GitHub language count](https://img.shields.io/github/languages/count/ItsEcholot/node-red-contrib-snmp-trap-listener)  
+![GitHub top language](https://img.shields.io/github/languages/top/ItsEcholot/node-red-contrib-snmp-trap-listener)
+
+### - Dependencies
+![GitHub package.json dependency version (prod)](https://img.shields.io/github/package-json/dependency-version/ItsEcholot/node-red-contrib-snmp-trap-listener/net-snmp)  
+![GitHub package.json dependency version (prod)](https://img.shields.io/github/package-json/dependency-version/ItsEcholot/node-red-contrib-snmp-trap-listener/moment)
+
+### - License
+![GitHub](https://img.shields.io/github/license/ItsEcholot/node-red-contrib-snmp-trap-listener)
--- snmp-trap-listener.js
+++ snmp-trap-listener.js
@@ -1,106 +1,260 @@
-const util              = require('util');
-const snmp              = require('snmpjs');
-const moment            = require('moment');
-const logTimeFormat     = 'D MMM HH:mm:ss - [[log]] ';
+const util			= require("util");
+const snmp			= require("net-snmp");
+const moment		= require("moment");
+const logTimeFormat	= "D MMM HH:mm:ss - [[log]] ";
 
 module.exports = (RED, debugSettings) => {
-    if (!RED && debugSettings) {
-        // Enter debug mode | Run outside of Node-RED
-        SnmpTrapListenerNode(debugSettings);
-    }
+	if (!RED && debugSettings) {
+		// Enter debug mode | Run outside of Node-RED
+		SnmpTrapListenerNode(debugSettings);
+	}
 
-    function SnmpTrapListenerNode(config) {
-        if (RED)
-            RED.nodes.createNode(this,config);
+	function SnmpTrapListenerNode(config) {
+		if (RED)
+			RED.nodes.createNode(this,config);
+		
+		this.receiver = null;
+		
+		let node = this;
+		
+		// Polyfills if RED is unavailable
+		if (!RED) {
+			node.log = msg => {
+				console.log(moment().format(logTimeFormat) + msg);
+			};
+			node.warn = msg => {
+				console.warn(moment().format(logTimeFormat) + msg);
+			};
+			node.error = msg => {
+				console.error(moment().format(logTimeFormat) + msg);
+			};
+			node.status = options => {
+				node.log("Debug options: " + JSON.stringify(options));
+				// console.log(moment().format(logTimeFormat) + "Debug options:");
+				// console.dir(options);
+			};
+			node.on = (event, cb) => {
+				node.log("Bound to RED event: " + event);
+				// console.log(moment().format(logTimeFormat) + "Bound to RED event " + event);
+				node.log("Would execute: " + cb);
+				// console.log(moment().format(logTimeFormat) + "Would execute");
+				// console.dir(cb);
+			};
+		}
+		
+		// Add a timeout to reset the status to green after x time (ms)
+		let timeoutStatus;
+		
+		if (!config.port) {
+			config.port = 162;
+			node.warn("No port set... Setting port to 162");
+			// console.warn(moment().format(logTimeFormat) + "No port set... Setting port to 162");
+		}
+		
+		if (config.snmpV1 == null) {
+			config.snmpV1 = true;
+		}
+		if (config.snmpV2 == null) {
+			config.snmpV2 = true;
+		}
+		if (config.snmpV3 == null) {
+			config.snmpV3 = true;
+		}
+		
+		node.status({fill: "yellow", shape: "ring", text: "connecting"});
 
-        let node = this;
-        // Polyfills if RED is unavailable
-        if (!RED) {
-            node.status = options => {
-                console.log(moment().format(logTimeFormat) + 'Debug options:');
-                console.dir(options);
-            };
-            node.on = (event, cb) => {
-                console.log(moment().format(logTimeFormat) + 'Bound to RED event ' + event);
-                console.log(moment().format(logTimeFormat) + 'Would execute');
-                console.dir(cb);
-            };
-            node.warn = msg => {
-                console.warn(moment().format(logTimeFormat) + msg);
-            };
-        }
-        let timeoutStatus;
-        if (!config.port) {
-            config.port = 162;
-            node.warn(moment().format(logTimeFormat) + 'No port set... Setting port to 162');
-        }
-        node.status({fill: 'yellow', shape: 'dot', text: 'connecting'});
-        const trapd = snmp.createTrapListener();
+		// Default options
+		var options = {
+			port: parseInt(config.port),
+			disableAuthorization: !(config.community),
+			engineID: "8000B98380XXXXXXXXXXXX", // where the X's are random hex digits
+			transport: "udp4"
+		};
 
-        node.on('close', function() {
-            // Cleanup before node is being deleted.
-            trapd.close();
-        });
-
-        trapd.on('trap', msg => {
-            clearTimeout(timeoutStatus);
-            node.status({fill: 'blue', shape: 'dot', text: 'ready - trapped'});
-            timeoutStatus = setTimeout(() => {
-                node.status({fill: 'green', shape: 'dot', text: 'ready'});
-            }, 100);
-            console.log(moment().format(logTimeFormat) + 'Received trap from agent', msg.pdu.agent_addr);
-
-            if (config.community)
-                if (msg.community.toString() !== config.community) {
-                    console.log(moment().format(logTimeFormat) + `Received trap with non matching community string: Expected ${config.community} received ${msg.community.toString()}`);
-                    return;
-                }
-            if (config.ipfilter && config.ipmask)
-                if ((IPnumber(config.ipfilter) & IPmask(config.ipmask)) != IPnumber(msg.src.address)) {
-                    console.log(moment().format(logTimeFormat) + `Received trap with wrong ip according to filter settings: ${msg.src.address}`);
-                    return;
-                }
-
-            const payload = [];
-            const serializedMsg = snmp.message.serializer(msg)['pdu'];
-
-            serializedMsg.varbinds.forEach(variable => {
-                payload.push(variable);
-            });
-
-            if (payload.length > 0)
-                node.send({
-                    payload,
-                    enterprise: serializedMsg.enterprise,
-                    agent_addr: serializedMsg.agent_addr,
-                    generic_trap: serializedMsg.generic_trap,
-                    specific_trap: serializedMsg.specific_trap,
-                    time_stamp: serializedMsg.time_stamp,
-                    src: msg.src,
-                    version: msg.version
-                });
-            else
-                console.log(moment().format(logTimeFormat) + 'Trap had empty payload');
-        });
-
-        trapd.bind({family: 'udp4', port: parseInt(config.port)}, () => {
-            console.log(moment().format(logTimeFormat) + `Success binding to port ${config.port} and listening for traps`);
-            node.status({fill: 'green', shape: 'dot', text: 'ready'});
-        });
-    }
-
-    function IPnumber(IPaddress) {
-        let ip = IPaddress.match(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/);
-        if(ip) {
-            return (+ip[1]<<24) + (+ip[2]<<16) + (+ip[3]<<8) + (+ip[4]);
-        }
-        return null;
-    }
-
-    function IPmask(maskSize) {
-        return -1<<(32-maskSize)
-    }
-
-    if (RED)
-        RED.nodes.registerType("snmp-trap-listener", SnmpTrapListenerNode);
+		var callback = function (error, trap) {
+			if (error) {
+				node.debug("Oops: " + JSON.stringify(error));
+				if (error.name == "RequestFailedError") {
+					clearTimeout(timeoutStatus);
+					node.status({fill: "red", shape: "dot", text: "error - not trapped"});
+					timeoutStatus = setTimeout(() => {
+						node.status({fill: "green", shape: "dot", text: "ready"});
+					}, 1000);
+					node.log("Received invalid trap: " + error.message);
+					// console.log(moment().format(logTimeFormat) + "Received invalid trap: ", error.message);
+				} else if (error.errno == "EACCES"){
+					node.error("can't open port: " + error.port);
+					node.status({fill: "red", shape: "dot", text: "error - port unavailable"});
+				} else {
+					node.log("Error message: " + error.message);
+					// console.log(moment().format(logTimeFormat) + "Error: ", error);
+				}
+			} else {
+				node.debug("Received: " + JSON.stringify(trap));
+					
+				let trapType = snmp.PduType[trap.pdu.type] || "Unknown";
+				
+				if (((trap.pdu.type == snmp.PduType.Trap) && (config.snmpV1)) || ((trap.pdu.type == snmp.PduType.TrapV2) && ((config.snmpV2) || (config.snmpV3)))) {
+						
+					clearTimeout(timeoutStatus);
+					node.status({fill: "blue", shape: "dot", text: "ready - trapped"});
+					timeoutStatus = setTimeout(() => {
+						node.status({fill: "green", shape: "dot", text: "ready"});
+					}, 1000);
+					// console.log(moment().format(logTimeFormat) + "Received trap from agent", trap.rinfo.address);
+					if ((!config.ipfilter) || (!config.ipmask) || (CheckIpFilter(config.ipfilter, config.ipmask, trap.rinfo.address))) {
+						let payload = [];
+						trap.pdu.varbinds.forEach(variable => {
+							payload.push({
+								oid: variable.oid,
+								typename: snmp.ObjectType[variable.type],
+								value: variable.value,
+								string_value: variable.value.toString()
+							});
+						});
+						if (trap.pdu.type == snmp.PduType.Trap) {
+							node.log("Received " + trapType + " from " + trap.rinfo.address + ": " + trap.pdu.enterprise);
+							// console.log (now + ": " + trapType + ": " + trap.rinfo.address + " : " + trap.pdu.enterprise);
+							node.send({
+								payload,
+								enterprise: trap.pdu.enterprise,
+								agent_addr: trap.pdu.agentAddr,
+								generic_trap: trap.pdu.generic,
+								specific_trap: trap.pdu.specific,
+								time_stamp: trap.pdu.upTime,
+								src:{
+									family: trap.rinfo.family,
+									address: trap.rinfo.address,
+									port: trap.rinfo.port
+								},
+								version: 0,
+								string_version: trapType,
+								_msgid: trap._msgid
+							});
+						} else {
+							node.log("Received " + trapType + " from " + trap.rinfo.address + ": " + trap.pdu.varbinds[1].value);
+							// console.log (now + ": " + trapType + ": " + trap.rinfo.address + " : " + trap.pdu.varbinds[1].value);
+							node.send({
+								payload,
+								src:{
+									family: trap.rinfo.family,
+									address: trap.rinfo.address,
+									port: trap.rinfo.port
+								},
+								version: 1,
+								string_version: trapType,
+								_msgid: trap._msgid
+							});
+						}
+					} else {
+						clearTimeout(timeoutStatus);
+						node.status({fill: "red", shape: "dot", text: "error - filtered"});
+						timeoutStatus = setTimeout(() => {
+							node.status({fill: "green", shape: "dot", text: "ready"});
+						}, 1000);
+						node.log("Received trap with wrong ip according to filter settings: " + config.ipfilter + "/" + config.ipmask);
+						// console.log(moment().format(logTimeFormat) + `Received trap with wrong ip according to filter settings: ${trap.rinfo.address}`);
+					}
+				} else {
+					node.log("Received " + trapType + " from " + trap.rinfo.address);
+				}
+			}
+		};
+		
+		node.receiver = snmp.createReceiver (options, callback);
+		node.log("Listening for traps on port: " + config.port);
+		let authorizer = node.receiver.getAuthorizer ();
+		if (((config.snmpV1) || (config.snmpV2)) && (config.community)) {
+			node.debug("Adding Community: " + config.community);
+			authorizer.addCommunity(config.community);
+		}
+		if ((config.snmpV3) && (config.user_name)) {
+			let user_level;
+			if (config.user_authProtocol == "none") {
+				user_level = snmp.SecurityLevel.noAuthNoPriv;
+			} else if (config.user_privProtocol == "none") {
+				user_level = snmp.SecurityLevel.authNoPriv;
+			} else {
+				user_level = snmp.SecurityLevel.authPriv;
+			}
+			let user_authProtocol;
+			switch(config.user_authProtocol) {
+				case "md5":
+					user_authProtocol = snmp.AuthProtocols.md5;
+					break;
+				case "sha":
+					user_authProtocol = snmp.AuthProtocols.sha;
+					break;
+				default:
+					user_authProtocol = snmp.AuthProtocols.md5;
+					break;
+			}
+			let user_privProtocol;
+			switch(config.user_privProtocol) {
+				case "des":
+					user_privProtocol = snmp.PrivProtocols.des;
+					break;
+				case "aes":
+					user_privProtocol = snmp.PrivProtocols.aes;
+					break;
+				default:
+					user_privProtocol = snmp.PrivProtocols.des;
+					break;
+			}
+			let user = {
+				name: config.user_name,
+				level: user_level,
+				authProtocol: user_authProtocol,
+				authKey: config.user_authKey,
+				privProtocol: user_privProtocol,
+				privKey: config.user_privKey
+			};
+			node.debug("Adding User: " + JSON.stringify(user));
+			authorizer.addUser(user);
+		}
+		node.status({fill: "green", shape: "dot", text: "ready"});
+		
+		node.on("close", function() {
+			// Cleanup before node is being deleted.
+			node.log("Closing port: " + config.port);
+			node.status({fill: "gray", shape: "ring", text: "offline"});
+			node.receiver.close();
+		});
+	}
+	
+	// Function to filter IP address (>>> 0 is to keep data as unsigned32)
+	function CheckIpFilter(ipfilter,ipmask,ipaddress) {
+		let _ipfilter = ipfilter.match(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/);
+		let _ipaddress = ipaddress.match(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/);
+		let _ipmask = ipmask.match(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/);
+		if (_ipmask) {
+			_ipmask = (+_ipmask[1]<<24) + (+_ipmask[2]<<16) + (+_ipmask[3]<<8) + (+_ipmask[4]) >>> 0;
+		} else {
+			_ipmask =  -1 << (32 - parseInt(ipmask)) >>> 0;
+		}
+		if (_ipfilter && ipmask && _ipaddress) {
+			_ipfilter = (+_ipfilter[1]<<24) + (+_ipfilter[2]<<16) + (+_ipfilter[3]<<8) + (+_ipfilter[4]) >>> 0;
+			
+			let _ipfiltermin;
+			let _ipfiltermax;
+			
+			if (_ipmask == 0xffffffff) { // Netmask 32 is only 1 address
+				_ipfiltermin = _ipfilter >>> 0;
+				_ipfiltermax = _ipfilter >>> 0;
+			} else if (_ipmask == 0xfffffffe) { // Netmask 32 is only 2 address (normal calculation with min/max reversed
+				_ipfiltermin = ((_ipfilter | ~ _ipmask) - 1) >>> 0;
+				_ipfiltermax = ((_ipfilter & _ipmask) + 1) >>> 0;
+			} else {
+				_ipfiltermin = ((_ipfilter & _ipmask) + 1) >>> 0;
+				_ipfiltermax = ((_ipfilter | ~ _ipmask) - 1) >>> 0;
+			}
+			_ipaddress = (+_ipaddress[1]<<24) + (+_ipaddress[2]<<16) + (+_ipaddress[3]<<8) + (+_ipaddress[4]) >>> 0;
+			if ((_ipfiltermin <= _ipaddress) && (_ipaddress <= _ipfiltermax)) return true;
+			else return false;
+		}
+		return false;
+	}
+	
+	if (RED)
+		RED.nodes.registerType("snmp-trap-listener", SnmpTrapListenerNode);
 };
